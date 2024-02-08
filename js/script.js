aa4aed17f3a513d0e7b2ea08590afbfb3922e316
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-08 18:35:35 +0100

    fine esercizio con bonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 
 // -----------------------------------------------------------------------------------------
-
-
+// ELEMENTI 
 // prelevo il bottone 
 const buttonElement = document.querySelector("#generateBtn");
 // prelevo la tabella 
 const gridElement = document.querySelector("#grid");
 // prelevo il risultato 
 const resultElement = document.getElementById("result");
-
-
-
 // prelevo l'elemento con le varie difficoltà
 let selectedElement = document.getElementById("mySelect");
 
 
+// CLICK 
 // al click genero la tabella 
 buttonElement.addEventListener("click", 
     function(){
@@ -24,7 +21,6 @@ buttonElement.addEventListener("click",
         resultElement.innerText = "";
         
         let gridNumber;
-
         // - con difficoltà 1
         if (selectedElement.value == "easy"){
             // 10 x 10 
@@ -39,25 +35,19 @@ buttonElement.addEventListener("click",
         }else if (selectedElement.value == "hard"){
             // 7 x 7 
             gridNumber = 49;
-
-        // Altrimenti se non si seleziona 
-        }else{
-            alert("Selezionare una difficoltà")
         }
 
         // aggiungo la classe alla griglia per selezionare quale quadrato prelevare 
-        gridElement.className = selectedElement.value;
-
-        // aggiungo la classe alla mia griglia 
+        gridElement.className = selectedElement.value; 
         gridElement.classList.add("my_grid");
 
 
 
-        // mostro in pagina le caselle azzeccate 
+        // mostro in pagina il punteggio 
         // inizializzo lo score a zero 
         let score = 0;
 
-        // genero un array 
+        // genero un array per le bombe 
         const bombArray = []
 
         while (bombArray.length < 16){
@@ -82,14 +72,8 @@ buttonElement.addEventListener("click",
                 // se il numero è presente gli aggiungo la classe bomb 
                 newSquare.classList.add("bomb");
             }
-
-
-
-
             // lo inserisco nella griglia 
             gridElement.append(newSquare);
-
-
 
 
             // al click di questa casella aggiungi o rimuovi una classe 
@@ -98,38 +82,46 @@ buttonElement.addEventListener("click",
                     
                     // se è una bomba 
                     if(this.classList.contains("bomb")){
-                        // allora aggiungo lo sfondo rosso
-                        this.classList.add("defeat");
-
-                        // blocco le azioni sulla griglia  
+                        // allora aggiungo lo sfondo rosso e blocco le azioni sulla griglia 
+                        this.classList.add("defeat"); 
                         gridElement.classList.add("grid-block");
 
-                        // se perdi
+                        // scrivendo all'utente che ha perso
                         resultElement.innerText = "Hai perso, Il tuo punteggio è: " + score;
 
+                        // dopodichè mostro tutte le bombe
+                        // prelevo tutti gli elementi con classe bomb 
+                        let bombs = document.getElementsByClassName("bomb");
 
+                        // gli ciclo
+                        for (let j = 0; j < bombs.length; j++) {
+                            // per ognuno aggiungo la classe defeat 
+                            bombs[j].classList.add("defeat");
+                        }
                     }else {
                         
-                        // altrimenti coloro la casella 
+                        // altrimenti coloro la casella e le blocco per evitare punteggio in più
                         this.classList.add("active_square");
-                        // e le blocco 
                         newSquare.classList.add("square-block")
 
                         // aggiungo un punto e lo scrivo in pagina  
                         score = score + 1;
-                        resultElement.innerText = "Il tuo punteggio è: " + score;
+                        resultElement.innerText = "Il tuo punteggio:  " + score;
 
+                        // se il giocatore completa tutta la tabella 
                         if( score == gridNumber - bombArray.length){
-                            // mostro al giocatore che ha vinto e blocco la griglia 
+                            // mostro al giocatore che ha vinto e blocco la griglia, mostrando anche qua le bombe 
                             resultElement.innerText = "Congratulazioni hai vinto";
                             gridElement.classList.add("grid-block");
+
+                            let bombs = document.getElementsByClassName("bomb");
+                            for (let j = 0; j < bombs.length; j++) {
+                            bombs[j].classList.add("defeat");
+                        }
+
                         }
                         
                     }
-
-                    // in console 
-                    console.log(this.innerText);
-
                 }
                 )       
         }  
